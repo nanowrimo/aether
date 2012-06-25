b71539aff6a677754269d58d8969657e5799d8d1
Dan Duvall <dan_duvall@lettersandlight.org>
2012-06-25 10:59:18 -0700

    Set dev-filaments instance to use DNS promotion.

--- lib/aether/instance/dev_filaments.rb
+++ lib/aether/instance/dev_filaments.rb
@@ -2,7 +2,10 @@ module Aether
   module Instance
     class DevFilaments < Default
       def initialize(options = {})
-        super("dev-filaments", {:instance_type => "m1.medium"}.merge(options))
+        super("dev-filaments", {
+          :instance_type => "m1.medium",
+          :promote_by => :dns_alias
+        }.merge(options))
       end
     end
   end
