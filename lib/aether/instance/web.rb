8a38863defafa7f2d1005415e8af207432c2d6b2
Dan Duvall <dan_duvall@lettersandlight.org>
2012-10-02 16:07:25 -0700

    Fixed web instance.

--- lib/aether/instance/web.rb
+++ lib/aether/instance/web.rb
@@ -2,7 +2,11 @@ module Aether
   module Instance
     class Web < Default
       def initialize(options = {})
-        super("web", {:instance_type => "c1.medium", :image_name => "web"}.merge(options))
+        super("web", {
+          :instance_type => "c1.medium",
+          :image_name => "web",
+          :configure_by => nil
+        }.merge(options))
 
         @manage_dns = false
       end
