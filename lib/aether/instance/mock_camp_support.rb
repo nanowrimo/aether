11f76e15c5dc8d9677f17b10b346efb8c8903131
Dan Duvall <dan_duvall@lettersandlight.org>
2013-02-27 16:54:15 -0800

    Added promotion option to mock-camp-support.

--- lib/aether/instance/mock_camp_support.rb
+++ lib/aether/instance/mock_camp_support.rb
@@ -2,7 +2,7 @@ module Aether
   module Instance
     class MockCampSupport < Default
       self.type = "mock-camp-support"
-      self.default_options = {:instance_type => "m1.large"}
+      self.default_options = {:instance_type => "m1.large", :promote_by => :dns_alias}
     end
   end
 end
