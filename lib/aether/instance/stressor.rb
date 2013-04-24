2ebd851ac915437314a2b4f8bb86a3c3fef5f8f9
Dan Duvall <dan_duvall@lettersandlight.org>
2013-04-24 10:38:42 -0700

    Modified stressor-instance options to use smaller instance

--- lib/aether/instance/stressor.rb
+++ lib/aether/instance/stressor.rb
@@ -2,7 +2,7 @@ module Aether
   module Instance
     class Stressor < Default
       self.type = "stressor"
-      self.default_options = {:instance_type => "c1.xlarge", :promote_by => :dns_alias}
+      self.default_options = {:instance_type => "c1.medium", :promote_by => :dns_alias}
     end
   end
 end
